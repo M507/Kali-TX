2b6f635d713a704d74ec7d8e6f4e2e784c2f4cd9
M507 <mohammed10work@gmail.com>
2019-06-11 13:24:00 -0400

    Adding Pwsh

---
+++ scripts/install-powershell.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+wget http://ftp.us.debian.org/debian/pool/main/i/icu/libicu57_57.1-6+deb9u2_amd64.deb
+wget http://ftp.us.debian.org/debian/pool/main/i/icu/icu-devtools_57.1-6+deb9u2_amd64.deb
+wget http://ftp.us.debian.org/debian/pool/main/u/ust/liblttng-ust0_2.9.0-2+deb9u1_amd64.deb
+wget http://ftp.us.debian.org/debian/pool/main/libu/liburcu/liburcu4_0.9.3-1_amd64.deb
+wget http://ftp.us.debian.org/debian/pool/main/u/ust/liblttng-ust-ctl2_2.9.0-2+deb9u1_amd64.deb
+
+dpkg -i liburcu4_0.9.3-1_amd64.deb 
+dpkg -i liblttng-ust-ctl2_2.9.0-2+deb9u1_amd64.deb
+dpkg -i liblttng-ust0_2.9.0-2+deb9u1_amd64.deb
+dpkg -i libicu57_57.1-6+deb9u2_amd64.deb
+dpkg -i icu-devtools_57.1-6+deb9u2_amd64.deb
+# Install
+apt update && apt -y install curl gnupg apt-transport-https
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/powershell.list
+apt update -y
+apt -y install powershell
+
