d17556073ebc543ceee0bd80ca7351a2e5672c20
Mohammed <38591075+M507@users.noreply.github.com>
2019-06-17 11:33:32 -0400

    Update setup-aliases.sh

--- scripts/setup-aliases.sh
+++ scripts/setup-aliases.sh
@@ -2,5 +2,37 @@
 echo "
 alias sl='ls'
 
+## grep aliases
+alias grep="grep --color=auto"
+alias ngrep="grep -n"
+alias egrep="egrep --color=auto"
+alias fgrep="fgrep --color=auto"
+
+## List open ports
+alias ports="netstat -tulanp"
+
+## Extract file, example. "ex package.tar.bz2"
+ex() {
+  if [[ -f $1 ]]; then
+    case $1 in
+      *.tar.bz2) tar xjf $1 ;;
+      *.tar.gz)  tar xzf $1 ;;
+      *.bz2)     bunzip2 $1 ;;
+      *.rar)     rar x $1 ;;
+      *.gz)      gunzip $1  ;;
+      *.tar)     tar xf $1  ;;
+      *.tbz2)    tar xjf $1 ;;
+      *.tgz)     tar xzf $1 ;;
+      *.zip)     unzip $1 ;;
+      *.Z)       uncompress $1 ;;
+      *.7z)      7z x $1 ;;
+      *)         echo $1 cannot be extracted ;;
+    esac
+  else
+    echo $1 is not a valid file
+  fi
+}
+
+# ex() is copied from https://github.com/Raikia/Kali-Setup/blob/master/kali.py
 #Add here..
 " >> /etc/bash.bashrc
