f6c5f12a09ea8baad42cc8810ef16ab144cec0f2
Mohammed <38591075+M507@users.noreply.github.com>
2019-06-27 11:09:50 -0400

    Adding a conditional statement
    Adding a conditional statement, in case it has been installed before

--- scripts/install-dirsearch.sh
+++ scripts/install-dirsearch.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-cd /opt
-git clone https://github.com/maurosoria/dirsearch.git
-cd dirsearch
-ln -s dirsearch.py /usr/bin/dirsearch.py
+FILE=/opt/dirsearch/dirsearch.py
+if [ -f "$FILE" ]; then
+    	echo "$FILE exist"
+else
+	cd /opt
+	git clone https://github.com/maurosoria/dirsearch.git
+	cd dirsearch
+	ln -s dirsearch.py /usr/bin/dirsearch.py
+fi
