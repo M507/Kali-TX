ca892a129168719acfa17f07447ce70974979e4e
Mohammed <38591075+M507@users.noreply.github.com>
2019-06-27 11:28:41 -0400

    Adding --fix-broken command

--- scripts/install-powershell.sh
+++ scripts/install-powershell.sh
@@ -16,4 +16,6 @@ curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/powershell.list
 apt update -y
 apt -y install powershell
-
+# In case
+apt --fix-broken install
+apt -y install powershell
