d80e7df5d282e1af398742c2eac387008aead131
Mohammed <38591075+M507@users.noreply.github.com>
2019-06-27 13:25:35 -0400

    Update install-empire.sh

--- scripts/install-empire.sh
+++ scripts/install-empire.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 FILE=/opt/Empire/empire
+
 if [ -f "$FILE" ]; then
     echo "Empire has been installed"
 else
 	cd /opt
 	git clone https://github.com/EmpireProject/Empire.git
 	cd Empire
-	expect -c 'spawn ./setup/install.sh ; expect "server"; send "\r"; interact'
+	sed -i -e 's/choice = raw_input/choice="" #/g' setup/setup_database.py
+        expect -c 'spawn ./setup/install.sh ; expect "server"; send "\r"; interact'
 	ln -s empire /usr/bin/empire
 fi
