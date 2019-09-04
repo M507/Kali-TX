04d5683ca391c62b36ae4ebd4a71e54247488adb
Mohad <38591075+M507@users.noreply.github.com>
2019-09-04 17:52:32 -0400

    Update install-docker.sh

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -3,3 +3,5 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' > /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-get install docker-ce -y
+apt-get install docker -y
+apt-get install docker-compose -y
