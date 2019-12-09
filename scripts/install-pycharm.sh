d03733a94ebb8c36e58edb5d3aff71e5aa67f995
Mohad <38591075+M507@users.noreply.github.com>
2019-12-09 17:15:17 -0500

    Fixing a broken symlink on desktop
    https://github.com/M507/Kali-TX/issues/2#issue-534836833

--- scripts/install-pycharm.sh
+++ scripts/install-pycharm.sh
@@ -3,4 +3,4 @@ cd /opt
 wget https://download-cf.jetbrains.com/python/pycharm-community-2019.1.2.tar.gz 
 tar xvzf pycharm-community-2019.1.2.tar.gz
 rm pycharm-community-2019.1.2.tar.gz
-ln -s pycharm-community-2019.1.2/bin/pycharm.sh /root/Desktop/pycharm.sh
+ln -s /opt/pycharm-community-2019.1.2/bin/pycharm.sh /root/Desktop/pycharm.sh
